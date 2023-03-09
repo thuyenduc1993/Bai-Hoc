46fcd197822aacd3998cf461a0439a831bd607d4
itvn.data@gmail.com <itvn.data@gmail.com>
2023-03-09 10:59:41 +0700

    9

---
+++ TTngay9.js
@@ -0,0 +1,12 @@
+let a = 10
+let b = 20.5
+let c = true
+let d= 'Hà Nội'
+
+document.write('a=' +a)
+document.write('<br/>')
+document.write('b=' +b)
+document.write('<br/>')
+document.write('c=' +c)
+document.write('<br/>')
+document.write('d=' +d)
