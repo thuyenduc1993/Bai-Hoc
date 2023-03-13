ccd16e092feb1f860612b5954948781bf1eeaa56
itvn.data@gmail.com <itvn.data@gmail.com>
2023-03-13 15:53:40 +0700

    11

---
+++ M1-L10/BT Tuan3/N12-3/BTTyesno/Btyesno.js
@@ -0,0 +1,12 @@
+
+    function ys() {
+    alert("yes i love you")
+
+}
+    function noclick() {
+    let w = Math.random() * window.innerHeight;
+    let h = Math.random() * window.innerWidth;
+    document.getElementById("no").style.left = w + "px";
+    document.getElementById("no").style.top = h + "px";
+}
+
