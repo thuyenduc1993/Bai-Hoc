eefcda0f7df78175f149ab899592198784fff69f
itvn.data@gmail.com <itvn.data@gmail.com>
2023-04-03 15:39:12 +0700

    1

---
+++ MD2/T1/B1/BTKhai_bao_gia_tri.js
@@ -0,0 +1,9 @@
+function lastIndexOf(arr, elt, start = 2) {
+    for (let i = start - 1; i >= 0; i--)
+        if (arr[i] === elt) {
+            return i
+        } else {
+            return -1
+        }
+}
+    console.log(lastIndexOf([1, 2, 1, 2], 2));
