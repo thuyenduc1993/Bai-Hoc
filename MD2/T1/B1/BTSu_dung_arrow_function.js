39a82d8f0153068981be18193ed3fffab3f6a543
itvn.data@gmail.com <itvn.data@gmail.com>
2023-04-03 17:13:33 +0700

    1

---
+++ MD2/T1/B1/BTSu_dung_arrow_function.js
@@ -0,0 +1,56 @@
+const inventory = [
+
+    {type:   "machine", value: 5000},
+
+
+    {type:   "machine", value:  650},
+
+
+    {type:      "duck", value:   10},
+
+
+    {type: "furniture", value: 1200},
+
+
+    {type:   "machine", value:   77}
+
+
+];
+// function totalMachineValue(inventorys){
+//     let sum =0
+//     for (let i = 0; i < inventorys.length; i++) {
+//         if(inventorys[i].type === "machine") {
+//             sum += inventorys[i].value;
+//         }
+//     }
+//     return sum
+// }
+// console.log(totalMachineValue(inventory))
+
+
+
+
+
+// let totalMachineValue = inventory.filter((item) =>{
+//     if(item.type === "machine") {
+//         return item.value;
+//     }
+// }).reduce((a,v)=> a + v.value,0)
+// console.log(totalMachineValue);
+
+
+
+// let totalMachineValue = inventory.filter((item)=>{
+//     if (item.type === "machine"){
+//         return item.value;
+//     }
+// }).reduce((sum,item) => {
+//     return sum + item.value
+// },0);
+// console.log(totalMachineValue);
+
+
+let totalMachineValue = inventory
+    .filter((item)=>item.type=== "machine")
+    .reduce((sum,item)=>sum+item.value,0);
+console.log(totalMachineValue);
