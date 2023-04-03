56544d7674e887fb8c27ddbcb28ca43d3a0dab5d
itvn.data@gmail.com <itvn.data@gmail.com>
2023-04-03 15:12:00 +0700

    1

---
+++ MD2/T1/B1/TTSSES6.js
@@ -0,0 +1,44 @@
+// function sum(x, y, z) {
+//     return x + y + z;
+// }
+//
+// const numbers = [1, 2, 3];
+// console.log(sum(...numbers));  // 6
+//
+
+
+
+
+//
+// let a = [1, 2];
+// a.push.apply(a,[1, 2, 3]);
+// console.log(a); //  [1,2,1,2,3]
+
+
+
+
+
+// let a = [1, 2];
+// let b= [1,2,3];
+// a.push(...b);
+// console.log(a);// [1,2,1,2,3]
+
+
+
+// let a = [1, 2];
+// let b= [1,2,3];
+// a = [...b];
+// console.log(a); //[1,2,3]
+
+
+
+// let  obj1={a:1, b:2};
+// let  obj2 = {...obj1};
+// console.log(obj2); // {a:1, b:2}
+
+
+// let a = [1,2];
+// let b = [3,4,5];
+// let c = [...a,...b];
+// console.log(c);//[1,2,3,4,5]
+
