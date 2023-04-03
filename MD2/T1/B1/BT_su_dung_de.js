be3db3c70bc258681c4628f5ad04d007de0757eb
itvn.data@gmail.com <itvn.data@gmail.com>
2023-04-03 20:26:31 +0700

    1

---
+++ MD2/T1/B1/BT_su_dung_de.js
@@ -0,0 +1,31 @@
+let profile = {
+
+    fname: 'Code',
+
+
+    lname: 'Gym',
+
+
+    bday: new Date('1979-01-02')
+
+
+}
+
+
+
+// let {lname,fname} = profile;
+// console.log(lname);
+// console.log(fname);
+
+
+
+
+let fname = profile.fname;
+let lname = profile.lname;
+console.log(fname, lname);
+
+
+
+
+
+
