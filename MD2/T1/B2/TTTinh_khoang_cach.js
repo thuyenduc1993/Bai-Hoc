3f987f05256b3f1b39b0ab36a933c8cf57ec6554
itvn.data@gmail.com <itvn.data@gmail.com>
2023-04-04 10:15:59 +0700

    2

---
+++ MD2/T1/B2/TTTinh_khoang_cach.js
@@ -0,0 +1,16 @@
+class Point {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+    displayName= "Point";
+    distance(b) {
+        let dx = this.x - b.x;
+        let dy = this.y - b.y;
+        return Math.hypot(dx, dy);
+    }
+}
+let p1 = new Point(1, 5);
+let p2 = new Point(10, 10);
+console.log(p1.displayName);
+console.log(p1.distance(p2));
