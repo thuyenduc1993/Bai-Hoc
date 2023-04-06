48d0d582abacb98a7e7bb2372d40c267e3be6e8f
itvn.data@gmail.com <itvn.data@gmail.com>
2023-04-06 10:51:07 +0700

    4

--- MD2/T1/B4/TTtao_lop_intructor.js
+++ MD2/T1/B4/TTtao_lop_intructor.js
@@ -6,9 +6,27 @@ class Instructor {
     renderDetails() {
         console.log(`${this.name} - ${this.role}`);
     }
+    static helloWorld() {
+        console.log('Hi there');
+    }
+
+
+    static canTeach(instructor) {
+        return (instructor.role === 'classroom');
+    }
 }
 let juniorInstructor = new Instructor({ 'name' : 'Brian' });
 let seniorInstructor = new Instructor({ 'name' : 'Alice', "role" : "classroom" });
 
 juniorInstructor.renderDetails();
-// seniorInstructor.renderDetails();
+seniorInstructor.renderDetails();
+
+Instructor.helloWorld();
+console.log(
+    `${juniorInstructor.name} can teach: ${Instructor.canTeach(juniorInstructor)}`
+);
+
+// "Alice can teach: true"
+console.log(
+    `${seniorInstructor.name} can teach: ${Instructor.canTeach(seniorInstructor)}`
+);
