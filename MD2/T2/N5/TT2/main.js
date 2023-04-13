548ad939b6585b9a049dcaf9d7dba0d8d0424816
itvn.data@gmail.com <itvn.data@gmail.com>
2023-04-13 14:40:00 +0700

    5

---
+++ MD2/T2/N5/TT2/main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var LinkedList_1 = require("./LinkedList");
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.insertFirstNode(1);
+linkedList.insertFirstNode(2);
+linkedList.insertFirstNode(3);
+linkedList.insertFirstNode(30);
+linkedList.insertLastNode(40);
+console.log(linkedList.getSize());
+console.log(linkedList.readList());
