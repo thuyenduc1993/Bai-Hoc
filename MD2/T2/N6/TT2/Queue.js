5e26e582651a981580b6a46cd6d5e32e2aae103e
itvn.data@gmail.com <itvn.data@gmail.com>
2023-04-14 09:51:28 +0700

    6

---
+++ MD2/T2/N6/TT2/Queue.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Queue = void 0;
+var Queue = /** @class */ (function () {
+    function Queue() {
+        this.container = [];
+    }
+    Queue.prototype.enqueue = function (item) {
+        this.container.push(item);
+    };
+    Queue.prototype.dequeue = function () {
+        return this.container.shift();
+    };
+    Queue.prototype.size = function () {
+        return this.container.length;
+    };
+    return Queue;
+}());
+exports.Queue = Queue;
---
+++ MD2/T2/N6/TT2/main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Queue_1 = require("./Queue");
+var queue = new Queue_1.Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+console.log(queue.dequeue());
+console.log(queue.container);
